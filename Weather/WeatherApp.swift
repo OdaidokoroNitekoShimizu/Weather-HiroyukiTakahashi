00053aba07cf7a223ecefd39bbc510bab8f24585
Hiroyuki <lahm0107@gmail.com>
2025-11-09 08:34:34 +0900

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by hiroyuki takahashi on R 7/11/09.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
