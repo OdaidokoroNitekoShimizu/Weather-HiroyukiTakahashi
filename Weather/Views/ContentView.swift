68a74176a77160991b64f196f38e1eecba3f8c8c
Hiroyuki <lahm0107@gmail.com>
2025-11-09 18:20:46 +0900

    天気アプリ完成！

--- Weather/Views/ContentView.swift
+++ Weather/Views/ContentView.swift
@@ -6,28 +6,55 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     //APIへリクエストしたり、レスポンスの値を保持するオブジェクト
     @StateObject private var weathetVM = WeatherViewModel()
-
+    @StateObject var locationManager = LocationManager() // 位置情報管理のオブジェクト
     // 八幡平市大更の緯度・経度
     var lat: Double = 39.91167
     var lon: Double = 141.093459
     var body: some View {
-
+        
         NavigationStack{
             ScrollView{
-
+                
+                VStack(spacing: 20) {
+                    DailyWeatherView(weatherVM: weathetVM)
+                    HourlyWeatherView(weatherVM: weathetVM)
+                }
+                .padding()
+                
             }
-            .navigationTitle("ここに現在地を表示")//画面上部のタイトル
+            //            .navigationTitle("ここに現在地を表示")//画面上部のタイトル
+            .navigationTitle("現在地: \(locationManager.address)")
             .navigationBarTitleDisplayMode(.inline)//タイトルの表示方法の指定
-
+            
         }
         .padding()
         //Vstackが表示されたときに実行される処理、現れる処理
         .onAppear{
-            weathetVM.request3DaysForecast(lat:lat,lon:lon)
+            //            weathetVM.request3DaysForecast(lat:lat,lon:lon)
+            getWeatherForecast()
+        }
+        // スクロールビューを下に引っ張って実行
+        .refreshable {
+            getWeatherForecast()
+        }
+    }
+    
+    // 現在地の天気予報取得、上から順番に処理したいのでDispatchQueue.main.async使用、先にprintが出ないように
+    func getWeatherForecast() {
+        DispatchQueue.main.async {
+            if let location = locationManager.location {
+                let latitude = location.coordinate.latitude     // 緯度
+                let longitude = location.coordinate.longitude   // 軽度
+                weathetVM.request3DaysForecast(lat: latitude, lon: longitude) // 天気リクエスト
+                print("LAT:", latitude, "LON:", longitude)
+            } else {
+                print("getting weather is failed")
+            }
         }
     }
 }
---
+++ Weather/Views/HourlyWeatherView.swift
@@ -0,0 +1,122 @@
+//
+//  HourlyWeatherView.swift
+//  Weather
+//
+//  Created by hiroyuki takahashi on R 7/11/09.
+//
+
+import SwiftUI
+
+struct HourlyWeatherView: View {
+    let defaultDateStr = "----年--月--日" // 日付が取得できていないときに表示する文字
+    @ObservedObject var weatherVM: WeatherViewModel
+    var body: some View {
+
+        if let forecastsDay =  weatherVM.forecast?.forecastsDay {
+            ForEach(forecastsDay,id:\.self){ forecastDay in
+
+                // compactMapを使って[HourlyForecast] → [HourlyDisplayForecast] に変換
+                let hourlyDisplayForecasts = forecastDay.hour.compactMap { hourlyDisplayForecast in
+                    hourlyDisplayForecast.toDisplayFormat(hourlyForecast: hourlyDisplayForecast)
+                }
+
+                VStack {
+                    Divider()
+                    //                    Text("__年__月__日")
+                    Text(hourlyDisplayForecasts.first?.date ?? defaultDateStr)
+                        .padding(.vertical, 5)  //垂直方向に余白を5とる
+
+                    HStack(spacing: 5) {
+                        HourlyWeatherHeader()
+
+                        ScrollView(.horizontal) {
+                            HStack(spacing: 5) {
+                                ForEach(hourlyDisplayForecasts) { hourlyDisplayForecast in
+                                    //                                ForEach(0...23, id: \.self) { _ in
+                                    // MARK: 1時間分の表示
+                                    VStack(spacing: 10) {
+                                        // 時刻
+                                        //                                        Text("0")
+                                        Text(hourlyDisplayForecast.time)
+                                            .font(.subheadline)
+
+                                        // 天気アイコン
+                                        //                                        Image(systemName: "cloud.sun")
+                                        //                                            .resizable()
+                                        //                                            .scaledToFit()
+                                        //                                            .frame(width: 64, height: 64)
+
+                                        AsyncImageView(urlStr: "https:\(hourlyDisplayForecast.weatherIcon)")
+                                            .frame(maxWidth:64, maxHeight: 64)
+                                            .scaledToFit()
+
+                                        // 気温
+//                                        Text(20, format: .number)
+                                        Text(hourlyDisplayForecast.temperature, format: .number)
+                                            .font(.subheadline)
+
+                                        // 降水確率
+//                                        Text(50, format: .number)
+                                        Text(hourlyDisplayForecast.chanceOfRain, format: .number)
+                                            .font(.subheadline)
+                                    }
+                                    .frame(width: ScreenInfo.width / 9)
+
+                                }
+                            }
+                            .padding()
+                            .frame(height: 180)
+                            .background(Color.gray.opacity(0.2))  // 背景色グレー
+                            .clipShape(.rect(cornerRadius: 10))  // 角丸に切り取る
+
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+
+
+    }
+}
+
+// MARK: - 時間毎の予報のヘッダー
+struct HourlyWeatherHeader: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 12) {
+            Text("時刻(時)")
+                .font(.caption)
+
+            Text("天気")
+                .font(.caption)
+                .frame(maxHeight: 40)
+
+            Text("気温(℃)")
+                .font(.caption)
+
+            Text("降水確率(%)")
+                .font(.caption2)
+        }
+        .frame(width: 65, height: 180)
+        .background(Color.blue.opacity(0.1))
+        .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+#Preview {
+    @Previewable @StateObject var weatherVM = WeatherViewModel()
+    // 八幡平市大更の緯度・経度
+    let lat: Double = 39.91167
+    let lon: Double = 141.093459
+
+    HourlyWeatherView(weatherVM: weatherVM)
+        .onAppear {
+            weatherVM.request3DaysForecast(lat: lat, lon: lon)
+        }
+}
+
+#Preview("ContentView") {
+    ContentView()
+}
